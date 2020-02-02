bdc85c196ffd8c862a7e8228879cb0d2cc17d77f
Riad <riadhasan5666@gmail.com>
2020-02-03 01:56:47 +0600

    created model product and review

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,4 +16,10 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('products', 'ProductController');
+
+Route::group(['prefix' => 'products'], function () {
+    Route::apiResource('{products}/reviews', 'ReviewController');
 });
